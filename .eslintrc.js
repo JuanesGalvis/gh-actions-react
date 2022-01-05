0221bb171f1a262190ad4ce4f3adfb0dbc7c09d4
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2022-01-04 19:52:21 -0500

    prueba ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-unused-vars": ["error", { "caughtErrors": "none" }],
+        "semi": [2, "always"]
+    }
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+/* eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+/* eslint-enable */
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
