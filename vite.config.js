bfa53c89b15681135b8b1ccded4bbcc28dd8fabf
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2022-01-04 19:13:11 -0500

    FIX base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://juanesgalvis.github.io/',
+  base: 'https://juanesgalvis.github.io/gh-actions-react/',
   plugins: [react()]
 })
